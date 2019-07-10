ef526179df5ab7c1f11c1c3df184296a9b4d3049
f-person <agakaryan.arshak@gmail.com>
2019-07-10 18:13:45 +0400

    Implement getBooks and getBooks

--- main.go
+++ main.go
@@ -1,51 +1,74 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
+// Book struct
 type Book struct {
-	Id     string  `json:"id"`
+	ID     string  `json:"id"`
 	Isbn   string  `json:"isbn"`
 	Title  string  `json:"title"`
 	Author *Author `json:"author"`
 }
 
+// Author struct
 type Author struct {
 	FirstName string `json:"firstname"`
 	LastName  string `json:"lastname"`
 }
 
+// init books
+var books []Book
+
 // get all books
 func getBooks(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
 }
 
 // get single book
 func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // get params
 
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
 }
 
 // create a new book
 func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
 }
 
 // udate a book
 func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
 }
 
 // delete a book
 func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
 }
 
 func main() {
 	// init router
 	r := mux.NewRouter()
+
+	// mock data
+	books = append(books, Book{ID: "1", Isbn: "1-4302-1948-3", Title: "Coders at work: Reflections on the Craft of Programming", Author: &Author{FirstName: "Peter", LastName: "Seibel"}})
+	books = append(books, Book{ID: "2", Isbn: "1-59327-007-0", Title: "Hacking: The Art of Exploitation", Author: &Author{FirstName: "Jon", LastName: "Erickson"}})
 
 	// route handlers / endpoints
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
@@ -55,5 +78,4 @@ func main() {
 	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", r))
-
 }
